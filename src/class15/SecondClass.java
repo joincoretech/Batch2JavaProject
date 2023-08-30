76dbafa7a5ac58c36254e7080a5fa4902c41dab8
azizullah.ayoub <azizullahayoubi19@gmail.com>
2023-08-29 21:47:13 -0400

    class 15 code

---
+++ src/class15/SecondClass.java
@@ -0,0 +1,16 @@
+package class15;
+
+public class SecondClass {
+
+    String var1;
+    String var2;
+
+    public void method1(){
+        System.out.println("this is my name "+var1);
+    }
+
+    public void method2(){
+        System.out.println("this is var2 "+ var2);
+    }
+
+}
---
+++ src/class15/SecondClassTester.java
@@ -0,0 +1,26 @@
+package class15;
+
+public class SecondClassTester {
+
+
+    public static void main(String[] args) {
+
+        SecondClass obj=new SecondClass();
+
+        obj.var1="Aslam";
+        obj.var2="Nasratullah";
+
+        obj.method1();
+        obj.method2();
+
+        SecondClass obj2=new SecondClass();
+
+        obj2.var1="Masodi";
+        obj2.var2="Iqbal";
+
+        obj2.method1();
+        obj2.method2();
+
+
+    }
+}
